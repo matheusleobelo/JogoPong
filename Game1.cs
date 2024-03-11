eab8517cdae3d8726c8448b1bb33027be9b25d89
matheusleobelo <bipoladogamer@gmail.com>
2024-03-11 11:36:32 -0300

    Mensagem do commit

---
+++ Game1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MyGame.Sprites;
+
+namespace MyGame {
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D Mesa;
+    private Texture2D Bola;
+
+    Player player;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        Mesa = Content.Load<Texture2D>("Mesa");
+        Bola = Content.Load<Texture2D>("Bola");
+
+        player = new Player(Content.Load<Texture2D>("Barra"));
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        player.Update();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(Mesa, new Rectangle(0, 0, 800, 480), Color.White);
+        _spriteBatch.Draw(Bola, new Vector2(40, 40), Color.White);
+        player.Draw(_spriteBatch);
+        
+        _spriteBatch.End();
+
+
+
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
+
+}
---
+++ Sprites/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyGame.Sprites
+{
+    internal class Player
+    {
+        Texture2D texture;
+        Vector2 position;
+        Vector2 velocity;
+        float speed;
+
+        public Player(Texture2D texture)
+        {
+            this.texture = texture;
+            speed = 4;
+        }
+
+        public void Update()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                velocity.Y -= 1;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                velocity.Y += 1;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                velocity.X += 1;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                velocity.X -= 1;
+            }
+
+            if (velocity != Vector2.Zero)
+                velocity.Normalize();
+
+            position += velocity * speed;
+            velocity = Vector2.Zero;
+        }
+            
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+
+    }
+}
